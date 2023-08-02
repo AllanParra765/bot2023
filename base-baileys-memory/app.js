d40a2b1622449cb1935426f33c46ed43081b3b1d
AllanParra765 <96440100+AllanParra765@users.noreply.github.com>
2023-08-01 19:31:16 -0600

    cambio de memory con capture:true
    Nuevo bot de prueba de memory para captura cx con capture:true

--- base-baileys-memory/app.js
+++ base-baileys-memory/app.js
@@ -1,71 +1,26 @@
-const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot');
-const QRPortalWeb = require('@bot-whatsapp/portal');
-const MySQLAdapter = require('@bot-whatsapp/database/mysql');
-const axios = require('axios'); // Importamos la biblioteca Axios
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
 
-/**
- * Declaramos las conexiones de MySQL
- */
-const MYSQL_DB_HOST = 'localhost';
-const MYSQL_DB_USER = 'root';
-const MYSQL_DB_PASSWORD = 'root';
-const MYSQL_DB_NAME = 'bd_bot';
-const MYSQL_DB_PORT = '8889';
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
 
-/**
- * Función para obtener los enlaces de interés utilizando Axios
- */
-async function obtenerEnlacesDeInteres() {
-  try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    return response.data;
-  } catch (error) {
-    console.error('Error al obtener los enlaces:', error.message);
-    return [];
-  }
-}
 
-// ...
-
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-  .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
-  .addAnswer(
-    async () => {
-      try {
-        // Llamamos a la función para obtener los enlaces de interés utilizando Axios
-        const enlacesDeInteres = await obtenerEnlacesDeInteres();
-
-        return [
-          'te comparto los siguientes links de interes sobre el proyecto',
-          `👉 *doc* para ver la documentación ${enlacesDeInteres[0]?.title || ''}`,
-          `👉 *gracias*  para ver la lista de videos ${enlacesDeInteres[1]?.title || ''}`,
-          '👉 *discord* unirte al discord',
-        ];
-      } catch (error) {
-        console.error('Error al obtener los enlaces:', error.message);
-        return [];
-      }
-    }
-  );
-
-// ...
+const flowPrincipal = addKeyword('hola').addAnswer('Indica cual es tu email',{capture:true}, (ctx) => {
+    console.log('👉 Informacion del contexto: ', ctx)
+})
 
 const main = async () => {
-  const adapterDB = new MySQLAdapter({
-    host: MYSQL_DB_HOST,
-    user: MYSQL_DB_USER,
-    database: MYSQL_DB_NAME,
-    password: MYSQL_DB_PASSWORD,
-    port: MYSQL_DB_PORT,
-  });
-  const adapterFlow = createFlow([flowPrincipal]);
-  const adapterProvider = createProvider();
-  createBot({
-    flow: adapterFlow,
-    provider: adapterProvider,
-    database: adapterDB,
-  });
-  QRPortalWeb();
-};
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow([flowPrincipal])
+    const adapterProvider = createProvider(BaileysProvider)
 
-main();
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
